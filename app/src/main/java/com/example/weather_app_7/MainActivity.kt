ce34fc65f08d75bdb8fee06e9d3ec21dbeb3c842
Farhhd <zazasihamid081@gmail.com>
2024-04-23 14:47:24 +0600

    Initial commit

---
+++ app/src/main/java/com/example/weather_app_7/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.weather_app_7
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.example.weather_app_7.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+    }
+    private fun navigate() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.navigate(R.id.menuFragment)
+    }
+}
